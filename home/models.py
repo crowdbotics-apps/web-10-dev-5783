bc34c69111017a7520c83b9d8df7e421a3aa3c65
Crowdbotics <team@crowdbotics.com>
2020-06-10 07:24:32 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -8,6 +8,25 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    gfkhgfjghf = models.OneToOneField(
+        "home.HomePage",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_gfkhgfjghf",
+    )
+    gfjhgfjgf = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_gfjhgfjgf",
+    )
+    adsdfa = models.BigIntegerField(null=True, blank=True,)
+    sfef = models.ManyToManyField(
+        "home.HomePage", blank=True, related_name="customtext_sfef",
+    )
+    asaw = models.BigIntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
