debf3680cf4fa05b059443e69f0c2495a01aaa0b
Crowdbotics <team@crowdbotics.com>
2020-06-10 07:16:30 +0000

    Update models

--- home/migrations/0005_auto_20200610_0713.py
+++ home/migrations/0005_auto_20200610_0713.py
@@ -6,36 +6,15 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0004_auto_20200610_0711'),
+        ("home", "0004_auto_20200610_0711"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='customtext',
-            name='gfhgfjhfjhgf',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='hfkhgfhgf',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='hgfhgfkh',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='hgfhjgf',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='jhgjhgk',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='jhgkhgljhg',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='nvhgjfghfg',
-        ),
+        migrations.RemoveField(model_name="customtext", name="gfhgfjhfjhgf",),
+        migrations.RemoveField(model_name="customtext", name="hfkhgfhgf",),
+        migrations.RemoveField(model_name="customtext", name="hgfhgfkh",),
+        migrations.RemoveField(model_name="customtext", name="hgfhjgf",),
+        migrations.RemoveField(model_name="customtext", name="jhgjhgk",),
+        migrations.RemoveField(model_name="customtext", name="jhgkhgljhg",),
+        migrations.RemoveField(model_name="customtext", name="nvhgjfghfg",),
     ]
